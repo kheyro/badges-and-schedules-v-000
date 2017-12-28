fd6bcf7c2f4f956c0649cdcb0d6f117b0855cc0a
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-28 15:10:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,11 +5,16 @@ end
 def batch_badge_creator(attendees)
   badge_message = []
 
-  attendees.each{ |attendee| badge_message << badge_maker(attendee) }
+  attendees.each { |attendee| badge_message << badge_maker(attendee) }
 
   badge_message
 end
 
 def assign_rooms(attendees)
+  room = 1
+  attendees.each do |attendee|
+    "Hello, #{attendee}! You'll be assigned to room #{room}!"
+    room += 1
+  end
 
 end
