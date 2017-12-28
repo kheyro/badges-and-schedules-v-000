64b470d6209e94ff3d3549554415f66b4848dfb2
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-28 15:05:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  badge_message = []
+  
+  attendees.each{ |attendee| badge_message << badge_maker(attendee) }
 
+  badge_message
 end
